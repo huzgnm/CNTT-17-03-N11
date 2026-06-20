8c222221cd9be5dbb590513c39b91a07cc4ea864
huzgnm <huzgnm@gmail.com>
2026-06-20 03:11:59 +0000

    feat: move khau_hao, ngan_sach_mua_sam, bao_cao_tai_san into quan_ly_tai_chinh
    - New model tai_chinh.khau_hao: monthly depreciation, cron auto-run on 1st of month
    - New model tai_chinh.ngan_sach_mua_sam: purchase budget with monthly detail lines
    - New model tai_chinh.bao_cao_tai_san: asset summary report by category
    - Updated menu, security, manifest for quan_ly_tai_chinh

--- addons/quan_ly_tai_chinh/models/__init__.py
+++ addons/quan_ly_tai_chinh/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-from . import ngan_sach
-from . import phieu_thu_chi
-from . import bao_cao_tai_chinh
+from . import phieu_thu_chi, ngan_sach, bao_cao_tai_chinh
+from . import khau_hao_hang_thang, ngan_sach_mua_sam, bao_cao_tai_san
